8d9a6f44bf9f5b14d602b98e2da276e40d53d135
Johanna Marcela Orjuela Flórez <johannaof@hotmail.com>
2022-11-23 17:11:27 -0500

    Función de arrastrar y soltar para las tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './task/task';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 
  @Component({
    selector: 'app-root',
@@ -18,4 +19,24 @@ import { Task } from './task/task';
       description: 'Using Firebase and Angular create a Kanban app!'
     }
   ];
- }
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+}
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@
  import { MatIconModule } from '@angular/material/icon';
 import { TaskComponent } from './task/task.component';
 import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
  
  @NgModule({
    declarations: [AppComponent, TaskComponent],
@@ -13,7 +14,8 @@ import { MatCardModule } from '@angular/material/card';
     BrowserModule, 
     MatToolbarModule, 
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    DragDropModule
   ],
    providers: [],
    bootstrap: [AppComponent],
