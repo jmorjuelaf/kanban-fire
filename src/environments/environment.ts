7b39bbed7ed1ce3c2402c92af5474f6d9660045b
Johanna Marcela Orjuela Flórez <johannaof@hotmail.com>
2022-11-23 22:15:48 -0500

    despliegue_app

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyAneflYik1Acmw3nyU_MJpm07OQKcwwAeo",
+  authDomain: "kanbanfire-49e84.firebaseapp.com",
+  projectId: "kanbanfire-49e84",
+  storageBucket: "kanbanfire-49e84.appspot.com",
+  messagingSenderId: "1056588882578",
+  appId: "1:1056588882578:web:7529eb1ae5999ffabe7c4b",
+  measurementId: "G-77Q3LX516J",
+},
+};
